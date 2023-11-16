f8d47f003b4491e91011a81f62726f4a4fc583e5
RonRain <rarainero@gmail.com>
2023-11-16 09:34:29 -0500

    include navbar

---
+++ navbar.js
@@ -0,0 +1,17 @@
+const navbarContainer = document.getElementById('navbar')
+
+const navbarLinks = navbarContainer.querySelectorAll('a')
+navbarLinks.forEach(link => {
+  link.addEventListener('click', function(event){
+    event.preventDefault()
+    const targetId = this.getAttribute('href').substring(1)
+    const targetSection = document.getElementById(targetId)
+
+    if (targetSection){
+      window.scrollTo({
+        top: targetSection.offsetTop,
+        behavior: 'smooth'
+      })
+    }
+  })
+})
